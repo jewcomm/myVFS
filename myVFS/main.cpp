abbbb5f00b4a669e1d353538cb775cf9f9ab55d0
jewcomm <32877204+UB3DDE@users.noreply.github.com>
2022-06-13 19:26:36 +0300

    Make work with folder

--- myVFS/main.cpp
+++ myVFS/main.cpp
@@ -22,6 +22,8 @@ const uint32_t CLUSTER_SIZE = (16 * 1024);
 #define ITS_FOLDER 1
 #define ITS_FILE 0
 
+const uint32_t end_data_block = 0xffffffff;
+
 namespace TestTask
 {
 	std::vector <std::string> parsePath(const char* name);
@@ -41,9 +43,9 @@ namespace TestTask
 
 	struct IVFS
 	{
-		virtual File *Open( const char *name ) = 0; // Открыть файл в readonly режиме. Если нет такого файла или же он открыт во writeonly режиме - вернуть nullptr
-		/*virtual File* Create(const char* name) = 0; // Открыть или создать файл в writeonly режиме. Если нужно, то создать все нужные поддиректории, упомянутые в пути. Вернуть nullptr, если этот файл уже открыт в readonly режиме.
-		virtual size_t Read( File *f, char *buff, size_t len ) = 0; // Прочитать данные из файла. Возвращаемое значение - сколько реально байт удалось прочитать
+		//virtual File *Open( const char *name ) = 0; // Открыть файл в readonly режиме. Если нет такого файла или же он открыт во writeonly режиме - вернуть nullptr
+		virtual File* Create(const char* name) = 0; // Открыть или создать файл в writeonly режиме. Если нужно, то создать все нужные поддиректории, упомянутые в пути. Вернуть nullptr, если этот файл уже открыт в readonly режиме.
+		/*virtual size_t Read( File *f, char *buff, size_t len ) = 0; // Прочитать данные из файла. Возвращаемое значение - сколько реально байт удалось прочитать
 		virtual size_t Write( File *f, char *buff, size_t len ) = 0; // Записать данные в файл. Возвращаемое значение - сколько реально байт удалось записать
 		virtual void Close( File *f ) = 0; // Закрыть файл*/
 
@@ -56,12 +58,29 @@ namespace TestTask
 
 		std::set <File, FileCMP> openFiles;
 
+		int writeMETAFT(FILE* file) {
+			fseek(file, 0, SEEK_SET);
+			fwrite(VERSION, 1, 5, file);
+			fwrite(&CLUSTER_SIZE, sizeof(uint32_t), 1, file);
+			// вообще это костыль, наверное есть и лучше реализация записи 0 в файл, но лучше не придумал(
+			fseek(file, 0x20, SEEK_SET);
+			fwrite("!", 1, 1, file);
+
+			fseek(file, 0x20 + 0x10 + 0x08, SEEK_SET);
+			fwrite(&end_data_block, sizeof(uint32_t), 1, file);
+
+			fseek(file, 0xffffffff - 1, SEEK_SET);
+			fwrite("\0", 1, 1, file);
+
+			return 1;
+		}
+
 		int writeMETA(FILE* file) {
 			fseek(file, 0, SEEK_SET);
 			fwrite(VERSION, 1, 5, file);
 			fwrite(&CLUSTER_SIZE, sizeof(uint32_t), 1, file);
 			// вообще это костыль, наверное есть и лучше реализация записи 0 в файл, но лучше не придумал(
-			fseek(file, 0x1f, SEEK_SET);
+			fseek(file, 0xffffffff-1, SEEK_SET);
 			fwrite("\0", 1, 1, file);
 
 			return 1;
@@ -107,7 +126,28 @@ namespace TestTask
 			uint32_t F_CLAST;
 			uint32_t NEXT_REC;
 			uint32_t REAL_SIZE;
+
+			bool operator == (FILE_TABLE* ft2) {
+				if (this->FILE_NAME == ft2->FILE_NAME &&
+					this->DOT == ft2->DOT &&
+					this->EXTENSION == ft2->EXTENSION && 
+					this->META == ft2->META) return true;
+				else return false;
+			}
+
+			bool operator != (FILE_TABLE* ft2) {
+				return !(this == ft2);
+			}
+
 		};
+
+		uint32_t findFreeFT() {
+			char firstSymbName;
+			while (fread(&firstSymbName, sizeof(char), 1, dFT) && firstSymbName != '\0') {
+				fseek(dFT, 0x1f, SEEK_CUR);
+			}
+			return ((ftell(dFT) / 0x20) - 1);
+		}
 
 		void getFT(FILE_TABLE *FT, File info) {
 			fseek(dFT, FIRST_ELEM_OFFSET + info.FILE_ID * 0x20, SEEK_SET);
@@ -121,7 +161,7 @@ namespace TestTask
 		}
 
 		void getFT(FILE_TABLE* FT, uint32_t offset) {
-			fseek(dFT, offset * 0x20, SEEK_SET);
+			fseek(dFT, FIRST_ELEM_OFFSET + offset * 0x20, SEEK_SET);
 			fread(FT->FILE_NAME, sizeof(char), 12, dFT);
 			fread(&FT->DOT, sizeof(char), 1, dFT);
 			fread(FT->EXTENSION, sizeof(char), 3, dFT);
@@ -147,9 +187,73 @@ namespace TestTask
 			fwrite(&FT->NEXT_REC, sizeof(uint32_t), 1, dFT);
 			fwrite(&FT->REAL_SIZE, sizeof(uint32_t), 1, dFT);
 
-			return ((ftell(dFT) / 0x20) - 1);
+			return ((ftell(dFT) / 0x20) - 2);
 		}
 
+		uint32_t reWriteFT(FILE_TABLE* FT, uint32_t offset) {
+			fseek(dFT, FIRST_ELEM_OFFSET + offset * 0x20, SEEK_SET);
+
+			fwrite(FT->FILE_NAME, sizeof(char), 12, dFT);
+			fwrite(&FT->DOT, sizeof(char), 1, dFT);
+			fwrite(FT->EXTENSION, sizeof(char), 3, dFT);
+			fwrite(&FT->META, sizeof(uint32_t), 1, dFT);
+			fwrite(&FT->F_CLAST, sizeof(uint32_t), 1, dFT);
+			fwrite(&FT->NEXT_REC, sizeof(uint32_t), 1, dFT);
+			fwrite(&FT->REAL_SIZE, sizeof(uint32_t), 1, dFT);
+
+			return ((ftell(dFT) / 0x20) - 2);
+		}
+
+		uint32_t createFolder(FILE_TABLE* FT, uint32_t offset) { // возвращает номер записи созданной папки
+			fseek(dFT, FIRST_ELEM_OFFSET + offset * 0x20, SEEK_SET);
+			
+			FILE_TABLE* i = new FILE_TABLE;
+			getFT(i, offset); // получаем инфу о родительской папке
+			uint32_t curRec = offset;
+
+			if (offset == 0) { // если это папка - корень 
+				while (i->NEXT_REC != 0xFFFFFFFF) {
+					curRec = i->NEXT_REC;
+					getFT(i, i->NEXT_REC);
+					if (i == FT) {
+						printf("ITS EQUAL FOLDER");
+						return curRec;
+					}
+				}
+
+				uint32_t res = writeFT(FT);
+				i->NEXT_REC = res;
+				reWriteFT(i, curRec);
+				return res;
+			}
+
+			if (i->F_CLAST == 0) {
+				uint32_t res = writeFT(FT);
+				i->F_CLAST = res;
+				reWriteFT(i, curRec);
+				
+				delete(i);
+				return res;
+			} 
+			else {
+				getFT(i, i->F_CLAST);
+				curRec = i->F_CLAST;
+
+				while (i->NEXT_REC != 0xFFFFFFFF) {
+					curRec = i->NEXT_REC;
+					getFT(i, i->NEXT_REC);
+					if (i == FT) {
+						printf("ITS EQUAL FOLDER");
+						return curRec;
+					}
+				}
+
+				uint32_t res = writeFT(FT);
+				i->NEXT_REC = res;
+				reWriteFT(i, curRec);
+				return res;
+			}
+		}
 
 	public:
 		VFS(FILE* _dFT, FILE** _dCS) {
@@ -158,33 +262,101 @@ namespace TestTask
 				i = *_dCS++;
 			}
 
-			if (checkFILE_TABLE(dFT)) writeMETA(dFT);
+			if (checkFILE_TABLE(dFT)) writeMETAFT(dFT);
 			for (auto& i : dCT) {
 				if (checkFILE_TABLE(i)) writeMETA(i);
 			}
 		}
 
-		File* Open(const char* name);
-		/*File* Create(const char* name);
-		size_t Read(File* f, char* buff, size_t len);
+		//File* Open(const char* name);
+		File* Create(const char* name);
+		/*size_t Read(File* f, char* buff, size_t len);
 		size_t Write(File* f, char* buff, size_t len);
 		void Close(File* f);*/
 	};
 
-	File* VFS::Open(const char* name)
+	File* VFS::Create(const char* name)
 	{
 		std::vector <std::string> path = parsePath(name);
 
 		std::vector <std::string> fileName = parseFileName(path.back());
 		path.pop_back();
+
+		uint32_t curREC = 0;
+
+		if (path.size() > 0) {
+			FILE_TABLE* folder = new FILE_TABLE;
+
+			strncpy(folder->FILE_NAME, (path[0].c_str()), 12);
+			folder->DOT = 0;
+			strncpy(folder->EXTENSION, "\0\0\0", 3);
+			folder->META = ITS_FOLDER;
+			folder->F_CLAST = (uint32_t)0;
+			folder->NEXT_REC = 0xFFFFFFFF;
+			folder->REAL_SIZE = 0;
+
+			curREC = createFolder(folder, 0);
+			delete(folder);
+		}
 		
+		// ищем последний файл в текущей директории
 		FILE_TABLE* i = new FILE_TABLE;
-		for (i->NEXT_REC = 1; i->NEXT_REC != 0xFFFFFFFF; getFT(i, i->NEXT_REC));
+
+		getFT(i, curREC);
+
+		if (curREC != 0) { // если создаем файл не в корне
+			if (i->F_CLAST != 0) { // если папка не пуста
+				getFT(i, i->F_CLAST);
+			}
+			else {
+				FILE_TABLE* temp = new FILE_TABLE;
+				strncpy(temp->FILE_NAME, (fileName[0].c_str()), 12);
+				temp->DOT = '.';
+				if (fileName.size() == 2) {
+					strncpy(temp->EXTENSION, fileName[1].c_str(), 3);
+				}
+				else {
+					strncpy(temp->EXTENSION, "\0\0\0", 3);
+				}
+				temp->META = ITS_FILE;
+				temp->F_CLAST = (uint32_t)0;
+				temp->NEXT_REC = (uint32_t)0xFFFFFFFF;
+				temp->REAL_SIZE = (uint32_t)0;
+
+				uint32_t res = writeFT(temp);
+
+				getFT(i, curREC);
+				i->F_CLAST = (uint32_t)res;
+				reWriteFT(i, curREC);
+
+				delete(temp);
+				delete(i);
+
+				openFiles.insert({ res, ITS_FILE });
+
+				File* t = new File;
+
+				t->FILE_ID = res;
+				t->MODE = ITS_FILE;
+
+				return t;
+			}
+		}
+
+		while (i->NEXT_REC != 0xFFFFFFFF) {
+			curREC = i->NEXT_REC;
+			getFT(i, i->NEXT_REC);
+		}
 
 		FILE_TABLE* temp = new FILE_TABLE;
-		strncpy(temp->FILE_NAME, "gedt", 12);
+		strncpy(temp->FILE_NAME, (fileName[0].c_str()), 12);
 		temp->DOT = '.';
-		strncpy(temp->EXTENSION, "ged", 3);
+		if (fileName.size() == 2) {
+			strncpy(temp->EXTENSION, fileName[1].c_str(), 3);
+		}
+		else {
+			strncpy(temp->EXTENSION, "\0\0\0", 3);
+		}
 		temp->META = ITS_FILE;
 		temp->F_CLAST = (uint32_t)0;
 		temp->NEXT_REC = (uint32_t)0xFFFFFFFF;
@@ -192,7 +364,12 @@ namespace TestTask
 
 		uint32_t res = writeFT(temp);
 
+		getFT(i, curREC);
+		i->NEXT_REC = (uint32_t)res;
+		reWriteFT(i, curREC);
+
 		delete(temp);
+		delete(i);
 
 		openFiles.insert({ res, ITS_FILE });
 
@@ -272,13 +449,11 @@ int main() {
 
 	TestTask::File* t;
 
-	t = firtsVFS.Open("4");
-	std::cout << t->FILE_ID << std::endl;
-
-/*	TestTask::parsePath("/loader/dendy/source/img/level/pic1.png");
-
-	TestTask::parseFileName("pic1");
-*/
+	firtsVFS.Create("file.txt");
+	firtsVFS.Create("example.txt");
+	firtsVFS.Create("dfb/file_st.txt");
+	firtsVFS.Create("txt/file_test.txt");
+	firtsVFS.Create("rex_dino");
 
 	fclose(FT);
 	for (auto& i : CS) {
